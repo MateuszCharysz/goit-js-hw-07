2f314a919bec0fab3169712930b2cb3a1f475957
MateuszCharysz <charyszek@interia.pl>
2023-02-04 20:02:20 +0100

    gallery works

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,13 +4,42 @@ import { galleryItems } from './gallery-items.js';
 const log = console.log
 log(galleryItems);
 
-{/* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+//DOM elements
+const gallery = document.querySelector(".gallery")
+log(gallery)
+
+//callback/functions
+const drawGallery = () => {
+    let protoGallery = ""
+    let protoGalleryItem =""
+    galleryItems.forEach((image) =>{
+        protoGalleryItem = `<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
     />
   </a>
-</div>; */}
+</div>`;
+protoGallery +=protoGalleryItem
+    })
+    gallery.insertAdjacentHTML('beforeend', protoGallery)
+}
+
+drawGallery()
+//Events
+
+
+
+// {/* <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>; */}
