f69eb7a4522f83cf53dc40a5fa087c43dab4de8f
MateuszCharysz <charyszek@interia.pl>
2023-02-04 20:33:43 +0100

    questions

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -29,17 +29,20 @@ protoGallery +=protoGalleryItem
 }
 
 drawGallery()
+
+
 //Events
 
+// Example
+// document.querySelector('button.image').onclick = () => {
+//   basicLightbox
+//     .create(
+//       `
+// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
+// 	`,
+//     )
+//     .show();
+// };
 
 
-// {/* <div class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-// </div>; */}
+
