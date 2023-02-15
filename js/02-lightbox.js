486b509282b46ceff964a314fc0c134b5c0f82d1
MateuszCharysz <charyszek@interia.pl>
2023-02-15 22:03:48 +0100

    starting work on lightbox set up

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+//help
+const log = console.log;
+// log(galleryItems);
 
-console.log(galleryItems);
+//DOM elements
+const gallery = document.querySelector('.gallery');
+// log(gallery);
+
+//callback/functions
+const drawGallery = () => {
+  let protoGallery = '';
+  let protoGalleryItem = '';
+  galleryItems.forEach(image => {
+    protoGalleryItem = `
+    <a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>
+`;
+    protoGallery += protoGalleryItem;
+  });
+  gallery.insertAdjacentHTML('beforeend', protoGallery);
+};
+
+drawGallery();
+//Events
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+  // log(event.target);
+  
+});
